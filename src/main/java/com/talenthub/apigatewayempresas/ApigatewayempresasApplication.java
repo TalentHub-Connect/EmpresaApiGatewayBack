2b74163f0353af6f129a2ebf5bc693afd03e4978
Felipe <juanfeariascastillo@live.com>
2024-04-17 16:10:54 -0500

    1.0.0

---
+++ src/main/java/com/talenthub/apigatewayempresas/ApigatewayempresasApplication.java
@@ -0,0 +1,13 @@
+package com.talenthub.apigatewayempresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayempresasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayempresasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talenthub/apigatewayempresas/ApigatewayempresasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talenthub.apigatewayempresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayempresasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
